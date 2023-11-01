245d19536eafe1e29bd8901cd09c52aad94305d6
Luc Schenherr <luc.schenherr@hotmail.fr>
2023-11-01 07:16:47 +0100

    Ajout du BrowserRouter sur App et non plus dans le routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { NavBarProvider } from './context/NavBar.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NavBarProvider>
-      <App />
-    </NavBarProvider>
+    <BrowserRouter>
+      <NavBarProvider>
+        <App />
+      </NavBarProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
